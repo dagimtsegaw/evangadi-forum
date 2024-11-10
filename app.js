aa45ec17a7838b80fbb6418c66813e2f4d4b3d8c
dagimtsegaw <dagatse577@gmail.com>
2024-11-10 17:40:32 +0300

    multiple changes

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
-const mysql2 = require("mysql2");
+const express = require("express");
+const app = express();
+const port = 5000;
 
-const dbConnection = mysql2.createPool({
-  user: "",
-  database: "",
-  host: "localhost",
-  password: "",
-  connectionLimit: 10,
+//register
+
+app.post("/api/users/register", (req, res) => {
+  res.send("register user");
+});
+//login
+app.post("/api/users/login", (req, res) => {
+  res.send("login user");
+});
+//check user
+app.get("/api/users/check", (req, res) => {
+  res.send("check user");
+});
+
+app.listen(port, (res, err) => {
+  if (err) {
+    console.log(err.message);
+  } else {
+    console.log(`listening to port ${port}`);
+  }
 });
